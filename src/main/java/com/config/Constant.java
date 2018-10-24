b250f414d9ee0585bdbc3453598aa0316bac6541
yangk@infogreat.cn <yangk@infogreat.cn>
2018-10-24 10:15:54 +0800

    init

---
+++ src/main/java/com/config/Constant.java
@@ -0,0 +1,15 @@
+package com.config;
+
+/**
+ * 项目中的常量定义类
+ */
+public class Constant {
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+     */
+    public static final String APP_KEY = "ding3izdkqaqamouhetu";
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+     */
+    public static final String APP_SECRET="GO5CDIFYdQZd3VNJnTq1c4ELQ9S3_YyhrtuWm8noNcVFPmlo5mVDZvJgzqxhfJGP";
+}
